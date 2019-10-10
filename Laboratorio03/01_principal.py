63c174eb712184925ea22a3426ef898914c389ae
Paolo Bejarano <31755218+paolobejarano@users.noreply.github.com>
2019-10-10 13:44:45 -0500

    Update 01_principal.py

--- Laboratorio03/01_principal.py
+++ Laboratorio03/01_principal.py
@@ -1,3 +1,4 @@
+from producto import Producto
 from cliente import Cliente
 
 '''
@@ -5,16 +6,13 @@ cliente = Cliente.obtener_usuario("luis.medina.delgado@hotmail.com")
 cliente.estado = "Z"
 cliente.actualizar_datos()
 print(cliente)
-'''
 
 # Crear cuenta
-cliente_new = Cliente(email='mauroleonpayano@gmail.com', password="demo")
-cliente_new.crear_cuenta()
-#print(cliente_new.crear_cuenta())
-
+cliente_new = Cliente(email="mauroleonpayano@gmail.com", password="demo")
+print(cliente_new.crear_cuenta())
 '''
+
 producto = Producto("demo", "demo", 17.3, 0.1)
 producto.estado = "P"
 producto.actualizar_datos()
 print(producto.agregar_producto())
-'''
